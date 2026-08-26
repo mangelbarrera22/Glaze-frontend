d375a1699107cb389cfa3419ec5d83bdf5b1a21b
mangelbarrera22 <mangelbarrera@ucundinamarca.edu.co>
2026-08-26 13:48:04 -0500

    Agregar ruta raiz al App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ProductoDetalle from "./components/ProductoDetalle";
 import Favoritos from "./components/Favoritos";
 import HistorialCompras from "./components/HistorialCompras";
 import Perfil from "./components/Perfil";
-import Soporte  from "./components/Soporte";
+import Soporte from "./components/Soporte";
 import FAQ from "./components/FAQ";
 import Publicar from "./components/Publicar";
 import MiCatalogo from "./components/MiCatalogo";
@@ -17,49 +17,30 @@ import EditarProducto from "./components/EditarProducto";
 import MisVentas from "./components/MisVentas";
 
 function App() {
-
  return (
-
   <Router>
-
    <Routes>
+    {/* Ruta raíz para solucionar la pantalla en blanco */}
+    <Route path="/" element={<Login />} />
 
     <Route path="/login" element={<Login />} />
-
     <Route path="/registro" element={<Register />} />
-
     <Route path="/dashboard" element={<Dashboard />} />
-
     <Route path="/dashboardVendedor" element={<DashboardVendedor />} />
-
-     <Route path="/catalogo" element={<Catalogo/>}/>
-
-     <Route path="/producto/:id" element={<ProductoDetalle />} />
-
-     <Route path="/favoritos" element={<Favoritos/>} ></Route>
-     
-     <Route path="/historialCompras" element={<HistorialCompras/>} ></Route>
-
-     <Route path="/perfil" element={<Perfil/>} ></Route>
-
-     <Route path="/soporte" element={<Soporte/>} ></Route>
-
-      <Route path="/FAQ" element={<FAQ/>} ></Route>
-
-       <Route path="/Publicar" element={<Publicar/>} ></Route>
-
-       <Route path="/MiCatalogo" element={<MiCatalogo/>} ></Route>
-
-       <Route path="EditarProducto/:id_producto" element={<EditarProducto />} />
-
-       <Route path="/MisVentas" element={<MisVentas />} />
-
+    <Route path="/catalogo" element={<Catalogo />} />
+    <Route path="/producto/:id" element={<ProductoDetalle />} />
+    <Route path="/favoritos" element={<Favoritos />} />
+    <Route path="/historialCompras" element={<HistorialCompras />} />
+    <Route path="/perfil" element={<Perfil />} />
+    <Route path="/soporte" element={<Soporte />} />
+    <Route path="/FAQ" element={<FAQ />} />
+    <Route path="/Publicar" element={<Publicar />} />
+    <Route path="/MiCatalogo" element={<MiCatalogo />} />
+    <Route path="/EditarProducto/:id_producto" element={<EditarProducto />} />
+    <Route path="/MisVentas" element={<MisVentas />} />
    </Routes>
-
   </Router>
-
  );
-
 }
 
 export default App;
