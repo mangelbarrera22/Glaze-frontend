6f1f355619cbccc4e5f13459c944abc5fc53b293
mangelbarrera22 <mangelbarrera@ucundinamarca.edu.co>
2026-09-01 14:29:00 -0500

    actualizacion rutas back

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://glaze-backend-production-ad01.up.railway.app/api"
+ baseURL: "https://glaze-backend-production-ad01.up.railway.app/api"
 });
 
 export default API; 
